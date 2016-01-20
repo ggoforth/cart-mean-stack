ff4e9d4923fa49d008118ee5b4d9508a697af888
Greg Goforth <greg.goforth@gmail.com>
2016-01-20 15:46:49 -0800

    adds examples for 03

--- 01/examples/javascript/script.js
+++ 01/examples/javascript/script.js
@@ -1,1 +1,29 @@
-alert('this is external');
+var firstName = 'greg',
+  lastName = 'goforth',
+  fullName = firstName + ' ' + lastName,
+  otherName = lastName + ', ' + firstName,
+  address = '2625',
+  age = 36,
+  kids = 2,
+  ageAddress = age + address,
+  isHuman = true,
+  homework = null,
+  explodingCats = undefined;
+
+console.log(age + kids);
+console.log(ageAddress);
+console.log(fullName, otherName);
+
+
+
+console.log(firstName, lastName);
+console.log(address, age);
+console.log(isHuman);
+console.log(homework);
+console.log(explodingCats);
+
+
+
+
+
+
