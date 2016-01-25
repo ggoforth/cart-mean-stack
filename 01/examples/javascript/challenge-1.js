f3fc68779b4435704d505d443e1dfba74658af2c
Greg Goforth <greg.goforth@gmail.com>
2016-01-25 14:23:28 -0800

    adds control flow example

---
+++ 01/examples/javascript/challenge-1.js
@@ -0,0 +1,11 @@
+var name = prompt('What is your name');
+var email = prompt('What is your email?');
+var movie = prompt('What is your favorite movie');
+var things = {
+  name: name,
+  movie: movie,
+  email: email
+};
+console.log(things);
+
+
---
+++ 01/examples/javascript/math.js
@@ -0,0 +1,7 @@
+//prompt for a number
+var number = prompt('Enter a number');
+console.log(number);
+console.log(isNaN(parseInt(number)));
+
+//console.log(that number + 100)
+console.log(parseInt(number) + 100);
